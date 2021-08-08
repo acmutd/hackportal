d43e4e7ac88a7fbd359f002fba0e2078a1a77a7f
Willie Chalmers III <willie.chalmers@acmutd.co>
2021-08-07 23:19:04 -0700

    feat: Set up PWA service worker and manifest
    The public/manifest.json file contains information used during app install. The
    theme color for the app is #5D5FEF. At some point, this needs to be reflected in
    the theming system. The next-pwa library automatially sets up the service worker
    that responds to offline requests. The _app.tsx file now contains HTML meta
    information in the head of all pages. The service worker is disabled by default
    in development builds. To enable it, create a ENABLE_PWA environment variable
    and set it to true.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
-module.exports = {
+const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
+
+module.exports = withPWA({
   reactStrictMode: true,
   images: {
     domains: ['lh3.googleusercontent.com'],
   },
-};
+  pwa: {
+    dest: 'public',
+    runtimeCaching,
+    disable: !process.env.ENABLE_PWA && process.env.NODE_ENV === 'development',
+  },
+});
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
+import Head from 'next/head';
 import { AppProps } from 'next/dist/next-server/lib/router/router';
 import 'firebase/auth';
 import AppHeader from '../components/AppHeader';
+import { initFirebase } from '../lib/firebase-client';
 import { AuthProvider } from '../lib/user/AuthContext';
 import '../styles/globals.css';
 import '../styles/tailwind.css';
-import { initFirebase } from '../lib/firebase-client';
 
 initFirebase();
 
@@ -17,6 +18,26 @@ initFirebase();
 function PortalApp({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+
+        <title>HackPortal</title>
+        <meta name="description" content="Your all-in-one guide to this hackathon." />
+
+        {!process.env.ENABLE_PWA && process.env.NODE_ENV === 'development' && (
+          <link rel="manifest" href="/manifest.json" />
+        )}
+
+        <link href="/icons/favicon-16x16.png" rel="icon" type="image/png" sizes="16x16" />
+        <link href="/icons/favicon-32x32.png" rel="icon" type="image/png" sizes="32x32" />
+        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
+        <meta name="theme-color" content="#5D5FEF" />
+      </Head>
       <div className="min-h-screen flex flex-col bg-gray-50">
         <AppHeader />
         <Component {...pageProps} />
