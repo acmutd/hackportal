7d7f6ddf6101b5ca7ca2b7b83052a0644ddda92e
nam-t24 <namtruong831@gmail.com>
2021-10-03 20:53:36 -0500

    feat(app, about, dashboard header): Created/edited all headers
    Dashboard and About created under both sections. App header changes included
    more subsections and small design changes

---
+++ components/DashboardHeader.tsx
@@ -0,0 +1,40 @@
+import Link from 'next/link';
+import React from 'react';
+
+/**
+ * A dashboard header.
+ */
+export default function DashboardHeader() {
+  return (
+    <>
+      <header className="top-0 sticky flex flex-row justify-between p-2 md:p-4 bg-white shadow-md items-center">
+        <div className="flex justify-center flex-wrap md:text-xl text:xs font-header md:text-left">
+          <Link href="/dashboard/">
+            <a>
+              <span className="inline md:invisible"></span>
+              <a className="md:mr-4 mr-2">Hack Center</a>
+            </a>
+          </Link>
+          <Link href="/dashboard/scan-in">
+            <a>
+              <span className="inline md:invisible"></span>
+              <a className="md:mx-4 mr-2">Scan-In</a>
+            </a>
+          </Link>
+          <Link href="/dashboard/hackerpack">
+            <a>
+              <span className="inline md:invisible"></span>
+              <a className="md:mx-4 mr-2">HackerPack</a>
+            </a>
+          </Link>
+          <Link href="/dashboard/submit">
+            <a>
+              <span className="inline md:invisible"></span>
+              <a className="md:mx-4 mr-2">Submit a Project</a>
+            </a>
+          </Link>
+        </div>
+      </header>
+    </>
+  );
+}
