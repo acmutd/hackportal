efa3c42f3bd19532a5a7697b923c4c1322ad65c5
nam-t24 <namtruong831@gmail.com>
2021-10-06 18:48:40 -0500

    feat: Created Hack Center outline
    Added all subsection and layouts to the hackcenter. All parts of the page are
    filled and are easily scrollable and responsive.

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import React from 'react';
+import Link from 'next/link';
 import DashboardHeader from '../../components/DashboardHeader';
 import { useUser } from '../../lib/profile/user-data';
 import { useAuthContext } from '../../lib/user/AuthContext';
@@ -13,10 +14,19 @@ export default function Dashboard() {
   const { isSignedIn } = useAuthContext();
   const user = useUser();
   const role = user.permissions?.length > 0 ? user.permissions[0] : '';
+  // Change this to check-in condition instead of signed in
   const checkin =
-    !user || !isSignedIn
-      ? 'It looks like you&apos;re not checked in! Please click here to check in to the event.'
-      : 'You are successfully checked in!';
+    !user || !isSignedIn ? (
+      <p>
+        It looks like you&apos;re not checked in! Please click{' '}
+        <Link href="/dashboard/scan-in" passHref>
+          <u>here</u>
+        </Link>{' '}
+        to check in to the event.
+      </p>
+    ) : (
+      'You are successfully checked in!'
+    );
 
   return (
     <div className="flex flex-wrap flex-grow">
@@ -46,9 +56,74 @@ export default function Dashboard() {
 
         <div className="lg:text-xl text-md bg-indigo-100 p-4 rounded-md mb-6">{checkin}</div>
 
-        <div className="flex">
-          <div className="w-3/5 bg-green-100">Spotlight</div>
-          <div className="w-2/5 bg-red-100">Announcements</div>
+        <div className="flex flex-wrap my-16">
+          <div className="md:w-3/5 w-screen h-96">
+            <h1 className="md:text-3xl text-xl font-black">Spotlight</h1>
+            <h3 className="md:text-xl text-md font-bold my-3">2 events are happening right now!</h3>
+            <div className="h-72 bg-gray-100 w-5/6"></div>
+          </div>
+
+          <div className="md:w-2/5 w-screen h-96">
+            <h1 className="md:text-3xl text-xl font-black">Announcements</h1>
+            <div id="announcement-items" className="overflow-y-scroll h-9/10">
+              <div
+                id="announcement-content"
+                className="bg-purple-200 md:min-h-1/4 min-h-1/2 rounded-lg p-3"
+              >
+                iluashdgilbngburiglbena
+              </div>
+              <p className="text-right">1:12 PM</p>
+              <div
+                id="announcement-content"
+                className="bg-purple-200 md:min-h-1/4 min-h-1/2 rounded-lg p-3"
+              >
+                iluashdgilbngburiglbena
+              </div>
+              <p className="text-right">1:12 PM</p>
+              <div
+                id="announcement-content"
+                className="bg-purple-200 md:min-h-1/4 min-h-1/2 rounded-lg p-3"
+              >
+                iluashdgilbngburiglbena
+              </div>
+              <p className="text-right">1:12 PM</p>
+              <div
+                id="announcement-content"
+                className="bg-purple-200 md:min-h-1/4 min-h-1/2 rounded-lg p-3"
+              >
+                iluashdgilbngburiglbena
+              </div>
+              <p className="text-right">1:12 PM</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="my-16">
+          <h1 className="md:text-3xl text-xl font-black">Mentor Center</h1>
+          <p className="my-3">
+            Mentors are available 24/7 in ECSW 2.414! You may also see some walking around the
+            building in
+            <b> purple </b>
+            shirts. We also have the following virtual judging rooms available right now:
+          </p>
+          <div className=" flex overflow-x-scroll w-full">
+            <div className="h-80 min-w-64 bg-red-200 rounded-2xl mx-4"></div>
+            <div className="h-80 min-w-64 bg-red-200 rounded-2xl mx-4"></div>
+            <div className="h-80 min-w-64 bg-red-200 rounded-2xl mx-4"></div>
+            <div className="h-80 min-w-64 bg-red-200 rounded-2xl mx-4"></div>
+            <div className="h-80 min-w-64 bg-red-200 rounded-2xl mx-4"></div>
+            <div className="h-80 min-w-64 bg-red-200 rounded-2xl mx-4"></div>
+          </div>
+        </div>
+
+        <div className="flex flex-wrap h-96 my-16">
+          <div className="md:w-3/5 w-screen bg-green-100">
+            <h1 className="md:text-3xl text-xl font-black">Your Saved Events</h1>
+          </div>
+          <div className="md:w-2/5 w-screen bg-purple-200">
+            <h1 className="md:text-3xl text-xl font-black">Your Team</h1>
+            <div className="h-3/5 p-5 md:text-xl text-lg">Hackergang</div>
+          </div>
         </div>
       </section>
     </div>
