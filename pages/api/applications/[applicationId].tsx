f29a7e180ac90fa9cc730eb5977413c6315cb1db
Nicolas A. Burnett <nicolasandrewburnett@gmail.com>
2021-10-23 17:50:53 -0500

    feat(registration): Implement backend logic to fetch user data
    Added a function to handle authentication and document fetching for registered users.
    
    feat #6

--- pages/api/applications/[applicationId].tsx
+++ pages/api/applications/[applicationId].tsx
@@ -1,4 +1,81 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { auth, firestore } from 'firebase-admin';
+import initializeApi from '../../../lib/admin/init';
+
+initializeApi();
+
+const db = firestore();
+
+const APPLICATIONS_COLLECTION = '/registrations';
+
+const USERS_COLLECTION = '/users';
+
+/**
+ * Verifies whether the given token belongs to an admin user.
+ *
+ * @param token A token to verify
+ * @returns True if the user is authorized to perform admin data management.
+ */
+async function userIsAuthorized(token: string) {
+  // TODO: Check if token is from actual user using Admin API
+  // TODO: Check if token was revoked, and send an appropriate error to client
+  const payload = await auth().verifyIdToken(token);
+  return true;
+}
+
+function extractHeaderToken(input: string) {
+  const result = input;
+  return result;
+}
+
+/**
+ * Handles GET requests to /api/application/<id>.
+ *
+ * This returns the application the authorized user wants to see.
+ *
+ * @param req The HTTP request
+ * @param res The HTTP response
+ */
+async function handleGetApplication(req: NextApiRequest, res: NextApiResponse) {
+  // TODO: Handle user authorization
+  const {
+    query: { token, id },
+    headers,
+  } = req;
+
+  //
+  // Check if request header contains token
+  // TODO: Figure out how to handle the string | string[] mess.
+  const userToken = (token as string) || (headers['authorization'] as string);
+  // TODO: Extract from bearer token
+  // Probably not safe
+  if (!userIsAuthorized(userToken)) {
+    res.status(401).send({
+      type: 'request-unauthorized',
+      message: 'Request is not authorized to perform admin functionality.',
+    });
+  }
+  const userID = id as string;
+
+  try {
+    const application = await db.collection(APPLICATIONS_COLLECTION).doc(userID);
+    const data = await application.get();
+    if (!data.exists) {
+      res.status(404).json({
+        code: 'not-found',
+        message: 'Application ID invalid, or the user is not registered.',
+      });
+    } else {
+      res.status(200).json(data.data());
+    }
+  } catch (error) {
+    console.error('Error when fetching applications', error);
+    res.status(500).json({
+      code: 'internal-error',
+      message: 'Something went wrong when processing this request. Try again later.',
+    });
+  }
+}
 
 /**
  * Get application data.
