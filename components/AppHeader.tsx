fd4735a5f7c493fbd3f037160036d062f9bd5f01
Abdullah H <71054105+abdullah50053@users.noreply.github.com>
2022-01-08 22:57:39 -0600

    Update AppHeader.tsx
    Preserve mobile hamburger menu

--- components/AppHeader.tsx
+++ components/AppHeader.tsx
@@ -1,10 +1,7 @@
 import Link from 'next/link';
 import React from 'react';
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
-import ProfileDialog from './ProfileDialog';
-import { useUser } from '../lib/profile/user-data';
-import { useAuthContext } from '../lib/user/AuthContext';
 import MenuIcon from '@material-ui/icons/Menu';
+import ProfileDialog from './ProfileDialog';
 
 import { navItems } from '../lib/data';
 
@@ -12,97 +9,75 @@ import { navItems } from '../lib/data';
  * A global site header throughout the entire app.
  */
 export default function AppHeader() {
-  const { isSignedIn } = useAuthContext();
-  const user = useUser();
-
+  const [showMenu, setShowMenu] = React.useState(false);
   const [showProfileDialog, setShowProfileDialog] = React.useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
 
   const dismissDialog = () => {
     setShowProfileDialog(false);
   };
-
   const toggleDialog = () => {
     setShowProfileDialog(!showProfileDialog);
   };
 
   return (
     <>
-      <header className="top-0 sticky justify-between flex flex-row p-2 md:p-4 bg-indigo-100 items-center h-16 z-10">
-        <div className="flex w-6/12 align-middle items-center">
+      <header className="top-0 fixed justify-around flex flex-row p-2 min-w-[320px] w-screen bg-indigo-100 items-center h-16 z-10 md:justify-between md:p-4">
+        <div className="flex w-6/12 max-w-[156px] justify-between items-center md:max-w-full md:justify-start md:w-9/12">
           <Link href="/">
             <a
-              className="flex relative font-display self-center items-center after:absolute after:block after:right-0 after:w-6 after:h-6 after:rounded-full after:bg-gray-400"
+              className="flex order-2 z-[-2] relative ml-[-6px] font-display self-center items-center w-[112px] md:order-1 md:ml-0 md:w-[176px] after:absolute after:block after:right-0 after:w-4 after:h-4 md:after:w-6 md:after:h-6 after:rounded-full after:bg-gray-400"
               onClick={dismissDialog}
             >
-              <span className="md:text-2xl text-l font-black md:mr-10 mr-5">HackUTD VIII</span>
+              <span className="text-[16px] font-black md:z-0 md:text-2xl md:mr-10">
+                HackUTD VIII
+              </span>
             </a>
           </Link>
-          <div className="text-xs flex items-center ml-12 md:text-left">
+          {/* Smartphone nav */}
+          <div onClick={toggleMenu} className={'relative md:hidden'}>
+            <MenuIcon />
+            <ul
+              className={`${
+                showMenu ? 'translate-x-0' : '-translate-x-full'
+              } transform transition-all ease-out duration-300 flex w-6/12 h-screen border-2 border-black flex-col bg-white fixed top-0 left-0 z-[-1] pt-16`}
+            >
+              {navItems.map((item) => (
+                <Link key={item.text} href={item.path}>
+                  <a
+                    className="border-b-2 first:border-t-2 border-black p-4 py-6"
+                    onClick={dismissDialog}
+                  >
+                    <p className="text-sm font-bold">{item.text}</p>
+                  </a>
+                </Link>
+              ))}
+            </ul>
+          </div>
+          {/* PC nav */}
+          <div className="hidden text-xs order-2 md:flex items-center md:text-left lg:ml-12">
             {navItems.map((item) => (
               <Link key={item.text} href={item.path}>
                 <a onClick={dismissDialog}>
-                  <span className="md:invisible"></span>
-                  <a className="md:mx-4 text-sm font-bold">{item.text}</a>
+                  <p className="md:mx-4 text-sm font-bold">{item.text}</p>
                 </a>
               </Link>
             ))}
           </div>
-          {/* Menu dropdown for mobile */}
-          <div className="sm:hidden">
-            <div className="dropdown inline-block relative">
-              <button className="bg-gray-300 text-gray-700 font-semibold py-1 px-2 rounded inline-flex items-center">
-                <span className="mr-1">Menu</span>
-                <MenuIcon />
-              </button>
-              <ul className="dropdown-menu absolute hidden text-gray-700 pt-1">
-                <li className="">
-                  <Link href="/dashboard">
-                    <a className="rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap">
-                      Dashboard
-                    </a>
-                  </Link>
-                </li>
-                <li className="">
-                  <Link href="/sponsors">
-                    <a className="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap">
-                      Sponsors
-                    </a>
-                  </Link>
-                </li>
-                <li className="">
-                  <Link href="/schedule">
-                    <a className="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap">
-                      Schedule
-                    </a>
-                  </Link>
-                </li>
-                <li className="">
-                  <Link href="/about">
-                    <a className="rounded-b bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap">
-                      About
-                    </a>
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-          {/* End of main menu items */}
         </div>
-        <div className="flex flex-row-reverse md:text-xl text-s">
-          <div className="mx-4">
-            <button
-              className="SigninButton font-headerSigninButton font-header font-bold bg-white px-8 py-1 rounded-full border-2 border-black text-sm"
-              onClick={toggleDialog}
-            >
-              {!user || !isSignedIn ? 'Sign in' : 'Profile'}
-              {/* To Do: must fix profile pic button */}
-              {/* {!user || !isSignedIn ? 'Sign in' : <Image src={user.photoUrl} alt="Profile">} */}
-              {/* {clsx({'Sign in' : (!user || !isSignedIn)})} */}
-            </button>
-          </div>
+        <div className="flex lg:mr-8">
+          <button
+            className="font-header font-bold bg-white rounded-full border-2 border-black text-sm px-8 py-1"
+            onClick={toggleDialog}
+          >
+            Sign In
+          </button>
         </div>
         {showProfileDialog && <ProfileDialog onDismiss={dismissDialog} />}
       </header>
-    </>
-  );
+      </>
+    );
 }
