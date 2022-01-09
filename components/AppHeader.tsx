264ce016dc2492145228f175e38c90d1231aacbc
nam-t24 <namtruong831@gmail.com>
2022-01-09 15:27:20 -0600

    fix: Remove unnecessary function from app header

--- components/AppHeader.tsx
+++ components/AppHeader.tsx
@@ -4,7 +4,6 @@ import MenuIcon from '@material-ui/icons/Menu';
 import ProfileDialog from './ProfileDialog';
 import { useUser } from '../lib/profile/user-data';
 import { useAuthContext } from '../lib/user/AuthContext';
-import { getItemCount } from '../pages/dashboard/index';
 import { navItems } from '../lib/data';
 
 /**
@@ -56,10 +55,7 @@ export default function AppHeader() {
                   <a
                     className="border-b-2 first:border-t-2 border-black p-4 py-6"
                     // onClick={dismissDialog, getItemCount}
-                    onClick={() => {
-                      dismissDialog();
-                      getItemCount();
-                    }}
+                    onClick={dismissDialog}
                   >
                     <p className="text-sm font-bold">{item.text}</p>
                   </a>
@@ -71,12 +67,7 @@ export default function AppHeader() {
           <div className="hidden text-xs order-2 md:flex items-center md:text-left lg:ml-12">
             {navItems.map((item) => (
               <Link key={item.text} href={item.path}>
-                <a
-                  onClick={() => {
-                    dismissDialog();
-                    getItemCount();
-                  }}
-                >
+                <a onClick={dismissDialog}>
                   <p className="md:mx-4 text-sm font-bold">{item.text}</p>
                 </a>
               </Link>
