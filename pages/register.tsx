bcc52b663c4e5833a5d2eb6fe7ef2f569944b359
nam-t24 <namtruong831@gmail.com>
2022-02-22 19:10:16 -0600

    feat: Implement custom form validation
    Specified fields have validation and form error message shows up on submit.

--- pages/register.tsx
+++ pages/register.tsx
@@ -20,6 +20,7 @@ export default function Register() {
   const { user, hasProfile, updateProfile } = useAuthContext();
   const [resumeFile, setResumeFile] = useState<File | null>(null);
   const [loading, setLoading] = useState(true);
+  const [formValid, setFormValid] = useState(true);
 
   const checkRedirect = async () => {
     if (hasProfile) router.push('/profile');
@@ -85,6 +86,7 @@ export default function Register() {
         <meta name="description" content="Register for [HACKATHON NAME]" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <section id="jumbotron" className="p-2 px-6">
         <div className="max-w-4xl py-6 mx-auto flex flex-col items-center">
           <div className="registrationTitle text-4xl font-bold text-center">
@@ -96,6 +98,7 @@ export default function Register() {
           </div>
         </div>
       </section>
+
       <section className="flex justify-center">
         <Formik
           initialValues={{
@@ -125,43 +128,131 @@ export default function Register() {
             website: '',
             companies: [],
           }}
+          //validation
+          validate={(values) => {
+            const errors: any = {};
+            // empErrors for nested user object
+            let empErrors: any = {};
+            //first name validation
+            if (!values.user.firstName) {
+              empErrors.firstName = 'Required';
+              errors.user = empErrors;
+            }
+            //last name validation
+            if (!values.user.lastName) {
+              empErrors.lastName = 'Required';
+              errors.user = empErrors;
+            }
+            //email validation
+            if (!values.user.preferredEmail) {
+              empErrors.preferredEmail = 'Required';
+              errors.user = empErrors;
+            } else if (
+              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.user.preferredEmail)
+            ) {
+              empErrors.preferredEmail = 'Invalid email address';
+              errors.user = empErrors;
+            }
+            //age validation
+            if (!values.age) {
+              errors.age = 'Required';
+              console.log('required');
+            } else if (values.age < 1 || values.age > 100) {
+              console.log(values.age);
+              errors.age = 'Not a valid age';
+            }
+            //gender validation
+            if (!values.gender) {
+              errors.gender = 'Required';
+            }
+            //race validation
+            if (!values.race) {
+              errors.race = 'Required';
+            }
+            //ethnicity validation
+            if (!values.ethnicity) {
+              errors.ethnicity = 'Required';
+            }
+            //university validation
+            if (!values.university) {
+              errors.university = 'Required';
+            }
+            //major validation
+            if (!values.major) {
+              errors.major = 'Required';
+            }
+            //studyLevel validation
+            if (!values.studyLevel) {
+              errors.studyLevel = 'Required';
+            }
+            //hackathonExperience validation
+            if (!values.hackathonExperience && values.hackathonExperience !== 0) {
+              errors.hackathonExperience = 'Required';
+            } else if (values.hackathonExperience < 0 || values.hackathonExperience > 100) {
+              errors.hackathonExperience = 'Not a valid number';
+            }
+
+            return errors;
+          }}
           onSubmit={async (values, { setSubmitting }) => {
             await new Promise((r) => setTimeout(r, 500));
             handleSubmit(values);
             setSubmitting(false);
           }}
         >
-          {({ values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting }) => (
+          {({ values, handleChange, isValid, dirty }) => (
+            // Field component automatically hooks input to form values. Use name attribute to match corresponding value
+            // ErrorMessage component automatically displays error based on validation above. Use name attribute to match corresponding value
             <Form
               onKeyDown={onKeyDown}
               noValidate
               className="registrationForm flex flex-col max-w-4xl px-6 w-[56rem] text-lg"
             >
               <div className="text-2xl py-1 border-b-2 border-black mr-auto my-6">General</div>
-              <label htmlFor="firstName">*First Name</label>
+              <label htmlFor="firstName" className="mt-4">
+                *First Name
+              </label>
               <Field
                 id="firstName"
                 name="user.firstName"
-                className="border-2 border-gray-400 rounded-md p-1 mb-4"
+                className="border-2 border-gray-400 rounded-md p-1"
+              />
+              <ErrorMessage
+                name="user.firstName"
+                render={(msg) => <div className="text-red-600">{msg}</div>}
               />
 
-              <label htmlFor="lastName">*Last Name</label>
+              <label htmlFor="lastName" className="mt-4">
+                *Last Name
+              </label>
               <Field
                 id="lastName"
                 name="user.lastName"
-                className="border-2 border-gray-400 rounded-md p-1 mb-4"
+                className="border-2 border-gray-400 rounded-md p-1"
+              />
+              <ErrorMessage
+                name="user.lastName"
+                render={(msg) => <div className="text-red-600">{msg}</div>}
               />
 
-              <label htmlFor="email">*Email</label>
+              <label htmlFor="email" className="mt-4">
+                *Email
+              </label>
               <Field
                 id="email"
                 name="user.preferredEmail"
-                className="border-2 border-gray-400 rounded-md p-1 mb-4"
+                className="border-2 border-gray-400 rounded-md p-1"
+              />
+              <ErrorMessage
+                name="user.preferredEmail"
+                render={(msg) => <div className="text-red-600">{msg}</div>}
               />
 
-              <label htmlFor="email">*Age</label>
+              <label htmlFor="email" className="mt-4">
+                *Age
+              </label>
               <input
-                className="border-2 border-gray-400 rounded-md p-1 mb-4"
+                className="border-2 border-gray-400 rounded-md p-1"
                 name="age"
                 type="number"
                 min="1"
@@ -169,12 +260,18 @@ export default function Register() {
                 onChange={handleChange}
                 value={values.age}
               />
+              <ErrorMessage
+                name="age"
+                render={(msg) => <div className="text-red-600">{msg}</div>}
+              />
 
-              <label htmlFor="gender">*Gender</label>
+              <label htmlFor="gender" className="mt-4">
+                *Gender
+              </label>
               <Field
                 as="select"
                 name="gender"
-                className="border-2 border-gray-400 rounded-md p-1 mb-4 mr-auto"
+                className="border-2 border-gray-400 rounded-md p-1 mr-auto"
               >
                 <option value="" disabled selected></option>
                 <option value="Female">Female</option>
@@ -182,12 +279,18 @@ export default function Register() {
                 <option value="Other">Other</option>
                 <option value="notSay">Prefer not to say</option>
               </Field>
+              <ErrorMessage
+                name="gender"
+                render={(msg) => <div className="text-red-600">{msg}</div>}
+              />
 
-              <label htmlFor="race">*Race</label>
+              <label htmlFor="race" className="mt-4">
+                *Race
+              </label>
               <Field
                 as="select"
                 name="race"
-                className="border-2 border-gray-400 rounded-md p-1 mb-4 mr-auto"
+                className="border-2 border-gray-400 rounded-md p-1 mr-auto"
               >
                 <option value="" disabled selected></option>
                 <option value="Native American">Native American</option>
@@ -198,42 +301,58 @@ export default function Register() {
                 <option value="Other">Multiple / Other</option>
                 <option value="notSay">Prefer not to answer</option>
               </Field>
+              <ErrorMessage
+                name="race"
+                render={(msg) => <div className="text-red-600">{msg}</div>}
+              />
 
-              <label htmlFor="ethnicity">*Enthicity</label>
+              <label htmlFor="ethnicity" className="mt-4">
+                *Enthicity
+              </label>
               <Field
                 as="select"
                 name="ethnicity"
-                className="border-2 border-gray-400 rounded-md p-1 mb-4 mr-auto"
+                className="border-2 border-gray-400 rounded-md p-1 mr-auto"
               >
                 <option value="" disabled selected></option>
                 <option value="hispanic">Hispanic or Latino</option>
                 <option value="notHispanic">Not Hispanic or Latino</option>
               </Field>
+              <ErrorMessage
+                name="ethnicity"
+                render={(msg) => <div className="text-red-600">{msg}</div>}
+              />
 
               <div className="text-2xl py-1 border-b-2 border-black mr-auto my-6">School Info</div>
-              <label htmlFor="university">
+              <label htmlFor="university" className="mt-4">
                 *This event is for college students worldwide. Which university do you attend?
               </label>
               <Field
                 id="university"
                 name="university"
-                className="border-2 border-gray-400 rounded-md p-1 mb-4"
+                className="border-2 border-gray-400 rounded-md p-1"
+              />
+              <ErrorMessage
+                name="university"
+                render={(msg) => <div className="text-red-600">{msg}</div>}
               />
 
-              <label htmlFor="major">
+              <label htmlFor="major" className="mt-4">
                 *All majors are welcome at this event. What is your major?
               </label>
-              <Field
-                id="major"
+              <Field id="major" name="major" className="border-2 border-gray-400 rounded-md p-1" />
+              <ErrorMessage
                 name="major"
-                className="border-2 border-gray-400 rounded-md p-1 mb-4"
+                render={(msg) => <div className="text-red-600">{msg}</div>}
               />
 
-              <label htmlFor="studyLevel">*Current level of study</label>
+              <label htmlFor="studyLevel" className="mt-4">
+                *Current level of study
+              </label>
               <Field
                 as="select"
                 name="studyLevel"
-                className="border-2 border-gray-400 rounded-md p-1 mb-4 mr-auto"
+                className="border-2 border-gray-400 rounded-md p-1 mr-auto"
               >
                 <option value="" disabled selected></option>
                 <option value="freshman">Freshman</option>
@@ -242,15 +361,19 @@ export default function Register() {
                 <option value="senior">Senior</option>
                 <option value="grad">Graduate Student</option>
               </Field>
+              <ErrorMessage
+                name="studyLevel"
+                render={(msg) => <div className="text-red-600">{msg}</div>}
+              />
 
               <div className="text-2xl py-1 border-b-2 border-black mr-auto my-6">
                 Hackathon Experience
               </div>
-              <label htmlFor="hackathonExperience">
+              <label htmlFor="hackathonExperience" className="mt-4">
                 *How many hackathons have you attended before?
               </label>
               <input
-                className="border-2 border-gray-400 rounded-md p-1 mb-4"
+                className="border-2 border-gray-400 rounded-md p-1"
                 name="hackathonExperience"
                 type="number"
                 min="0"
@@ -258,12 +381,18 @@ export default function Register() {
                 onChange={handleChange}
                 value={values.hackathonExperience}
               />
+              <ErrorMessage
+                name="hackathonExperience"
+                render={(msg) => <div className="text-red-600">{msg}</div>}
+              />
 
-              <label htmlFor="softwareExperience">*Relative software-building experience:</label>
+              <label htmlFor="softwareExperience" className="mt-4">
+                *Relative software-building experience:
+              </label>
               <Field
                 as="select"
                 name="softwareExperience"
-                className="border-2 border-gray-400 rounded-md p-1 mb-4 mr-auto"
+                className="border-2 border-gray-400 rounded-md p-1 mr-auto"
               >
                 <option value="Beginner">Beginner</option>
                 <option value="Intermediate">Intermediate</option>
@@ -272,11 +401,13 @@ export default function Register() {
               </Field>
 
               {/*ORGANIZER CAN CUSTOMIZE DROPDOWN OPTIONS*/}
-              <label htmlFor="heardFrom">*Where did you hear about HackPortal?</label>
+              <label htmlFor="heardFrom" className="mt-4">
+                *Where did you hear about HackPortal?
+              </label>
               <Field
                 as="select"
                 name="heardFrom"
-                className="border-2 border-gray-400 rounded-md p-1 mb-4 mr-auto"
+                className="border-2 border-gray-400 rounded-md p-1 mr-auto"
               >
                 <option value="Instagram">Instagram</option>
                 <option value="Twitter">Twitter</option>
@@ -285,11 +416,13 @@ export default function Register() {
               </Field>
 
               <div className="text-2xl py-1 border-b-2 border-black mr-auto my-6">Event Info</div>
-              <label htmlFor="size">*Shirt Size</label>
+              <label htmlFor="size" className="mt-4">
+                *Shirt Size
+              </label>
               <Field
                 as="select"
                 name="size"
-                className="border-2 border-gray-400 rounded-md p-1 mb-4 mr-auto"
+                className="border-2 border-gray-400 rounded-md p-1 mr-auto"
               >
                 <option value="s">S</option>
                 <option value="m">M</option>
@@ -297,8 +430,10 @@ export default function Register() {
                 <option value="xl">XL</option>
               </Field>
 
-              <label htmlFor="dietary">Allergies / Dietary Restrictions:</label>
-              <div role="group" aria-labelledby="checkbox-group" className="flex flex-col mb-4">
+              <label htmlFor="dietary" className="mt-4">
+                Allergies / Dietary Restrictions:
+              </label>
+              <div role="group" aria-labelledby="checkbox-group" className="flex flex-col">
                 <label>
                   <Field type="checkbox" name="dietary" value="Vegan" />
                   &nbsp;Vegan
@@ -329,40 +464,48 @@ export default function Register() {
                 </label>
               </div>
 
-              <label htmlFor="accomodations">
+              <label htmlFor="accomodations" className="mt-4">
                 Anything else we can do to better accommodate you at our hackathon?
               </label>
               <Field
                 as="textarea"
                 name="accomodations"
                 placeholder="List any accessibility concerns here"
-                className="border-2 border-gray-400 rounded-md p-1 mb-4"
+                className="border-2 border-gray-400 rounded-md p-1"
               ></Field>
 
               <div className="text-2xl py-1 border-b-2 border-black mr-auto my-6">Sponsor Info</div>
-              <label htmlFor="github">Github:</label>
+              <label htmlFor="github" className="mt-4">
+                Github:
+              </label>
               <Field
                 id="github"
                 name="github"
-                className="border-2 border-gray-400 rounded-md p-1 mb-4"
+                className="border-2 border-gray-400 rounded-md p-1"
               />
 
-              <label htmlFor="linkedin">LinkedIn:</label>
+              <label htmlFor="linkedin" className="mt-4">
+                LinkedIn:
+              </label>
               <Field
                 id="github"
                 name="linkedin"
-                className="border-2 border-gray-400 rounded-md p-1 mb-4"
+                className="border-2 border-gray-400 rounded-md p-1"
               />
 
-              <label htmlFor="website">Personal Website:</label>
+              <label htmlFor="website" className="mt-4">
+                Personal Website:
+              </label>
               <Field
                 id="github"
                 name="website"
-                className="border-2 border-gray-400 rounded-md p-1 mb-4"
+                className="border-2 border-gray-400 rounded-md p-1"
               />
 
-              <label htmlFor="companies">Companies to send my resume to:</label>
-              <div role="group" aria-labelledby="checkbox-group" className="flex flex-col mb-4">
+              <label htmlFor="companies" className="mt-4">
+                Companies to send my resume to:
+              </label>
+              <div role="group" aria-labelledby="checkbox-group" className="flex flex-col">
                 <label>
                   <Field type="checkbox" name="companies" value="State Farm" />
                   &nbsp;State Farm
@@ -385,7 +528,7 @@ export default function Register() {
                 </label>
               </div>
 
-              <label>
+              <label className="mt-4">
                 Upload your resume:
                 <br />
                 <input
@@ -397,12 +540,18 @@ export default function Register() {
                 <br />
               </label>
 
-              <button
-                type="submit"
-                className="mr-auto my-8 cursor-pointer px-4 py-2 rounded-md bg-blue-200 hover:bg-blue-300"
-              >
-                Submit
-              </button>
+              <div className="my-8">
+                <button
+                  type="submit"
+                  className="mr-auto cursor-pointer px-4 py-2 rounded-md bg-blue-200 hover:bg-blue-300"
+                  onClick={() => setFormValid(!(!isValid || !dirty))}
+                >
+                  Submit
+                </button>
+                {!isValid && !formValid && (
+                  <div className="text-red-600">Error: The form has invalid fields</div>
+                )}
+              </div>
             </Form>
           )}
         </Formik>
