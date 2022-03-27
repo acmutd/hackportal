06c2872545dd3baf768fd36b74d4b8274f1f2a43
Mike Nguyen <mike.nguyen@acmutd.co>
2022-03-27 14:03:12 -0500

    fix: Removed decimal point values from bar chart

--- components/StatsBarChart.tsx
+++ components/StatsBarChart.tsx
@@ -16,12 +16,24 @@ interface StatsBarChartProps {
 }
 
 export default function StatsBarChart({ name, items }: StatsBarChartProps) {
+  const removeDecimalPointLabel = (props) => {
+    const { text } = props;
+    if (text % 1 !== 0) {
+      return null;
+    }
+    const newProps = {
+      ...props,
+      text: parseInt(props.text),
+    };
+    return <ValueAxis.Label {...newProps} />;
+  };
+
   return (
-    <div className="border-2 my-2 rounded-2xl p-6">
-      <Chart data={items}>
+    <div className="border-2 my-2 rounded-2xl md:p-6">
+      <Chart data={items} rotated={screen.width <= 768}>
         <Palette scheme={['#BF40BF', '#FFA500', '#FFFF00', '#00FF00', '#0000FF', '#FF0000']} />
         <ArgumentAxis />
-        <ValueAxis />
+        <ValueAxis labelComponent={removeDecimalPointLabel} />
 
         <BarSeries valueField="itemCount" argumentField="itemName" />
         <Title text={name} />
