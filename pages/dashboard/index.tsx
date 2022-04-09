c2e4955634531307fe7114d72c25feaad8a2a0bc
Nam Truong <namtruong831@gmail.com>
2022-04-08 22:39:29 -0500

    Refactor: Use state for challenge cards (#106)
    * feat: Make headers dynamic based on user signin + register
    
    App header and dashboard headers varies depending on user signin/register
    
    * feat: Add admin to header and redirect to home in questions/scanin page
    
    Admin option header only shows up if the user signed in is admin or super admin.
    A non signed in user will be redirected to home page when they try to access
    question/scanin page
    
    * refactor: Remove has profile constraints and add register msg to scan in
    
    * feat: Make dashboard hidden
    
    Dashboard redirects to home page if user if not signed in making it hidden
    
    * refactor: Change challanges to use state instead of hidden/block style

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 import DashboardHeader from '../../components/DashboardHeader';
 import { useUser } from '../../lib/profile/user-data';
 import { useAuthContext } from '../../lib/user/AuthContext';
--- pages/dashboard/questions.tsx
+++ pages/dashboard/questions.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 import AboutHeader from '../../components/AboutHeader';
 import AnsweredQuestion from '../../components/AnsweredQuestion';
 import ErrorList from '../../components/ErrorList';
@@ -16,6 +17,7 @@ import DashboardHeader from '../../components/DashboardHeader';
  * Route: /dashboard/questions
  */
 export default function QuestionsPage() {
+  const router = useRouter();
   const [loading, setLoading] = useState(true);
   const [errors, setErrors] = useState<string[]>([]);
   const [currentQuestion, setCurrentQuestion] = useState('');
