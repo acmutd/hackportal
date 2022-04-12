60bbf5b858c41b88c68a3e160148ebfe5c7c71cd
Mike Nguyen <mike.nguyen@acmutd.co>
2022-04-12 18:48:48 -0500

    feat: Added feature to export user data to csv file

---
+++ lib/csv/fields.ts
@@ -0,0 +1,8 @@
+export const csvHeaders = [
+  { label: 'First Name', key: 'user.firstName' },
+  { label: 'Last Name', key: 'user.lastName' },
+  { label: 'Email', key: 'user.preferredEmail' },
+  { label: 'University', key: 'university' },
+  { label: 'Major', key: 'major' },
+  { label: 'Study Level', key: 'studyLevel' },
+];
