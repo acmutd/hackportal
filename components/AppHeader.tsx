ea53cd723b5bfc634fbaf2d8829f73561d4ab59c
Abdullah H <71054105+abdullah50053@users.noreply.github.com>
2022-04-27 02:14:33 -0500

    bug(menu): Fix logo overlap with hamburger menu on mobile

--- components/AppHeader.tsx
+++ components/AppHeader.tsx
@@ -76,7 +76,7 @@ export default function AppHeader() {
       <header className="top-0 fixed justify-between flex flex-row w-full bg-indigo-100 items-center h-16 z-10 p-4">
         <div className="flex justify-between items-center md:max-w-full md:justify-start md:w-9/12">
           <Link href="/">
-            <a className="flex gap-2 order-2 relative ml-[-6px] font-display self-center items-center md:order-1 md:ml-0">
+            <a className="flex gap-2 order-2 relative ml-[6px] font-display self-center items-center md:order-1 md:ml-0">
               {/* !change src */}
               <Image src={'/assets/hp-logo.png'} width="30px" height="30px" />
               <span className="text-[16px] font-black md:z-0 md:text-2xl">HackPortal</span>
