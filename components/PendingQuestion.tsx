1822bf0d1343bd41917163dd2df33916f42176e3
nam-t24 <namtruong831@gmail.com>
2022-07-13 20:18:32 -0500

    chore: Apple config color changes to new develop commits

--- components/PendingQuestion.tsx
+++ components/PendingQuestion.tsx
@@ -16,6 +16,7 @@ export default function PendingQuestion({ question }: PendingQuestionProps) {
   return (
     <div className="flex flex-row items-center gap-x-2 my-3">
       <svg height="14" width="14">
+        {/* !change bullet color */}
         <circle cx="7" cy="7" r="5" fill="#F8ACFF" />
       </svg>
       <div className="rounded-lg w-full py-2 px-3 bg-complementary">
