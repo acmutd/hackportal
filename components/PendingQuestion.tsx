6748b7611aad05f066401e03ba267f49e403140b
nam-t24 <namtruong831@gmail.com>
2022-08-17 17:33:35 -0500

    feat: Design admin main page

--- components/PendingQuestion.tsx
+++ components/PendingQuestion.tsx
@@ -16,9 +16,9 @@ export default function PendingQuestion({ question }: PendingQuestionProps) {
   return (
     <div className="flex flex-row items-center gap-x-2 my-3">
       <svg height="14" width="14">
-        <circle cx="7" cy="7" r="5" fill="#F8ACFF" />
+        <circle cx="7" cy="7" r="5" fill="#9e73d2" />
       </svg>
-      <div className="rounded-lg w-full py-2 px-3" style={{ backgroundColor: '#FDECFF' }}>
+      <div className="rounded-lg w-full py-2 px-3" style={{ backgroundColor: '#9e73d2' }}>
         <h1 className="font-semibold">{question}</h1>
       </div>
     </div>
