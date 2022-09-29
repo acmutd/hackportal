6eed128162ab350523c04d0c43f2ee9ece829c34
Abdullah H <71054105+abdullah50053@users.noreply.github.com>
2022-09-29 17:46:32 -0500

    Revert "Refactor/change title image"

--- pages/index.tsx
+++ pages/index.tsx
@@ -218,18 +218,18 @@ export default function Home(props: {
               <Image src={'/assets/Satellite.png'} alt="Satellite" layout="fill" />
             </div>
           </div>
-          <div className="2xl:w-[60rem] 2xl:h-[27rem] md:w-[43rem] md:h-[22rem] sm:w-[30rem] sm:h-[17rem] w-[22rem] h-[12rem] relative z-10 md:mb-8 mb-6">
-            <Image src={'/assets/HackUTD-IX-Title-Date.png'} alt="Hero" layout="fill"></Image>
+          <div className="2xl:w-[60rem] 2xl:h-[25rem] md:w-[43rem] md:h-[18rem] sm:w-[27rem] sm:h-[15rem] w-[20rem] h-[10rem] relative z-10">
+            <Image src={'/assets/HackUTD-IX-Title.png'} alt="Hero" layout="fill"></Image>
           </div>
-          {/* <div className="dateGradient font-bold lg:text-5xl md:text-4xl text-3xl md:mt-8 mt-16 relative z-10">
+          <div className="dateGradient font-bold lg:text-5xl md:text-4xl text-3xl md:mt-8 mt-16 relative z-10">
             11.12 - 11.13
-          </div> */}
+          </div>
           <Link href="/register" passHref={true}>
             <div className="registerGlow relative z-10 cursor-pointer xl:px-12 xl:py-4 sm:px-8 sm:py-4 px-6 py-2 bg-gradient-to-b from-[#00D1FF] to-[#124866] rounded-full md:mt-8 mt-12 2xl:text-4xl lg:text-3xl sm:text-2xl text-xl font-medium">
               Register Now
             </div>
           </Link>
-          <p className="md:mt-14 mt-12 text-bold xl:text-6xl sm:text-4xl text-3xl flex relative z-10">
+          <p className="md:mt-16 mt-12 text-bold xl:text-6xl sm:text-4xl text-3xl flex relative z-10">
             T -
             <div className="lg:mx-5 md:mx-4 mx-2">
               <div>{countdownTime.countdownDays}</div>
