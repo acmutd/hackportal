6dd3035d4188fd43536af25b40de5377f9baef48
Abdullah H <71054105+abdullah50053@users.noreply.github.com>
2022-10-12 13:52:35 -0500

    Update profile.tsx

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -86,7 +86,7 @@ export default function ProfilePage() {
                     <h1 className="font-bold">{profile.studyLevel}</h1>
                   </div>
                   <div className="profile-view-color flex flex-col gap-y-2">
-                    <div className="font-bold text-xl">Team Color</div>
+                    <div className="font-bold text-xl">Your Color</div>
                     <h1 className="font-bold">{profile.user.color}</h1>
                   </div>
                 </div>
