0bc0a8ba65e15a7c1e90ac83721006cbc3d7f285
Mike Nguyen <mike.nguyen@acmutd.co>
2022-10-14 21:59:28 -0500

    fix: Fixed CSS round 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,6 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    screens: {
-      md: '835px',
-    },
     extend: {
       fontFamily: {
         sans: ['Montserrat'],
