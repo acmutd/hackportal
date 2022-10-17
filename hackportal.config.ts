ceeeaab1b29b25d51d79255dc61b4a1b0579e987
Abdullah Hasani <abdullah50053@gmail.com>
2022-10-17 13:42:16 -0500

    chore(config): Update cutoff epoch timestamp

--- hackportal.config.ts
+++ hackportal.config.ts
@@ -475,7 +475,7 @@ export const hackPortalConfig: HackPortalConfig = {
       },
     ],
   },
-  registrationCutoff: 1665945883640,
+  registrationCutoff: 1666069200,
 };
 
 //add any question data that your org would like to see on the admin stats page
