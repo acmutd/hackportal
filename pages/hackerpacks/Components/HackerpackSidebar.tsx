be2a1a2f2520389b0111a82e71f524b0dece8fe2
Michael Zhao <michaelzhao314@gmail.com>
2022-10-23 15:00:32 -0500

    feat: Add Notion hackerpack base support

--- pages/hackerpacks/Components/HackerpackSidebar.tsx
+++ pages/hackerpacks/Components/HackerpackSidebar.tsx
@@ -19,7 +19,7 @@ export default function HackerpackSidebar({ content }: { content: SidebarSection
 
       <section
         id="Sidebar"
-        className="hidden md:flex justify-center h-screen fixed top-16 border-r-2 border-gray-600 w-1/4 md:w-1/6 2xl:w-1/8 text-xs md:text-xs lg:text-sm overflow-auto"
+        className="hidden md:flex justify-center h-screen fixed top-16 border-r-2 border-gray-600 w-1/4 md:w-1/6 2xl:w-1/8 text-xs md:text-xs lg:text-sm overflow-auto text-white"
       >
         <section id="options" className="relative px-6 py-4">
           <div className="font-bold mb-3">HackerPack</div>
@@ -27,12 +27,22 @@ export default function HackerpackSidebar({ content }: { content: SidebarSection
             {/* Maps the sidebar-content.json file to a nested list */}
             {content.map((mainSection) => (
               <li key={mainSection.title}>
-                {mainSection.title}
+                <a
+                  className="bold text-white hover:text-blue-100 transition-all"
+                  href={mainSection.href}
+                >
+                  {mainSection.title}
+                </a>
                 {mainSection.sections ? (
                   <ul className="pl-4">
                     {mainSection.sections.map((subsection) => (
                       <li key={subsection.title}>
-                        <a href={subsection.href || '#'}>{subsection.title}</a>
+                        <a
+                          href={subsection.href || '#'}
+                          className="text-gray-400 hover:text-white transition-all"
+                        >
+                          {subsection.title}
+                        </a>
                       </li>
                     ))}
                   </ul>
