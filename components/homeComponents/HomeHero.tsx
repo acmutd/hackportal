c94254bc6535f3547b8c0ff2b40cb21cb666dc15
Geeth Gunnampalli <geeth.gunnampalli@gmail.com>
2023-02-14 10:28:31 -0600

    fix(home): Fix Styling

--- components/homeComponents/HomeHero.tsx
+++ components/homeComponents/HomeHero.tsx
@@ -10,9 +10,7 @@ export default function HomeHero() {
         style={{ minHeight: 480 }}
         className="max-w-4xl mx-auto flex flex-col justify-center items-center"
       >
-        <h1 className="text-center md:text-8xl text-6xl text-transparent text-primaryDark">
-          HackPortal
-        </h1>{' '}
+        <h1 className="text-center md:text-8xl text-6xl font-bold text-primaryDark">HackPortal</h1>{' '}
         {/* !change */}
         <p className="text-center my-4 font-semibold md:text-xl text-md text-primaryDark opacity-80">
           {' '}
