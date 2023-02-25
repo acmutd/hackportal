ca728fd210da3d345abe39c92ac78bb7d6093c6f
Amrit Rathie <amrit.rathie@gmail.com>
2023-02-25 15:57:06 -0800

    chore(appointment): Fix typing of change event
    It really should be some combination of `ScheduleEvent` and `AppointmentModel`,
    but I can't figure out how to combine the two. Until then, this works because
    `AppointmentModel` defines arbitrary extra fields in its definition.

--- pages/schedule/index.tsx
+++ pages/schedule/index.tsx
@@ -159,7 +159,7 @@ export default function Calendar(props: { scheduleCard: ScheduleEvent[] }) {
     ),
   );
 
-  const changeEventData = (data: ScheduleEvent) => {
+  const changeEventData = (data: AppointmentModel) => {
     const startDate = new Date(data.startDate);
     const endDate = new Date(data.endDate);
 
@@ -171,7 +171,7 @@ export default function Calendar(props: { scheduleCard: ScheduleEvent[] }) {
     });
     const dayString = dateFormatter.format(startDate);
 
-    const speakersData = data.speakers?.filter((speaker) => speaker.length !== 0);
+    const speakersData = data.speakers?.filter((speaker: string[]) => speaker.length !== 0);
 
     // format list of speakers of event, leaving blank if no speakers
     const speakerFormatter = new Intl.ListFormat('default', { style: 'long', type: 'conjunction' });
