86a82923d76a33ca3c0120f7b1849b3997287820
Amrit Rathie <amrit.rathie@gmail.com>
2023-02-25 14:34:50 -0600

    refactor(calendar): Format dates, times, and lists with `Intl` API
    Use built-in `Intl` API to format event date, event time range, and event
    speaker list. This gets rid of custom code and relies on browser APIs, enabling
    automatic internationalization with no overhead for this component. Also
    significantly reduces code size lol.

--- pages/schedule/index.tsx
+++ pages/schedule/index.tsx
@@ -161,7 +161,7 @@ export default function SchedulePage(props: { scheduleCard: ScheduleEvent[] }) {
     ),
   );
 
-  const changeEventData = (data: AppointmentModel) => {
+  const changeEventData = (data: ScheduleEvent) => {
     const startDate = new Date(data.startDate);
     const endDate = new Date(data.endDate);
 
@@ -173,12 +173,12 @@ export default function SchedulePage(props: { scheduleCard: ScheduleEvent[] }) {
     });
     const dayString = dateFormatter.format(startDate);
 
-    const speakersData = data.speakers?.filter((speaker: string[]) => speaker.length !== 0);
+    const speakersData = data.speakers?.filter((speaker) => speaker.length !== 0);
 
     // format list of speakers of event, leaving blank if no speakers
     const speakerFormatter = new Intl.ListFormat('default', { style: 'long', type: 'conjunction' });
     const speakerString =
-      speakersData?.length > 0 ? `Hosted by ${speakerFormatter.format(speakersData)}` : '';
+      speakersData?.length || 0 > 0 ? `Hosted by ${speakerFormatter.format(speakersData)}` : '';
     // format time range of event
     const timeFormatter = new Intl.DateTimeFormat('default', {
       hour: 'numeric',
