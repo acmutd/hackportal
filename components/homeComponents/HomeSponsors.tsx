0c16b71baa8770e2fe7387e2690a1cae9c3df6ae
nam-t24 <namtruong831@gmail.com>
2023-10-07 13:52:39 -0500

    feat: Add more sponsor logos and split non sponsors

--- components/homeComponents/HomeSponsors.tsx
+++ components/homeComponents/HomeSponsors.tsx
@@ -79,22 +79,52 @@ export default function HomeSponsors() {
             objectFit="contain"
           />
         </a>
-        <div className="relative w-[16rem] h-32">
+        <a
+          href={'https://geico.wd1.myworkdayjobs.com/External'}
+          target="_blank"
+          className="relative w-80 h-28"
+          rel="noreferrer"
+        >
+          <Image
+            src={'/assets/sponsorLogos/Geico.png'}
+            alt="Geico"
+            layout="fill"
+            objectFit="contain"
+          />
+        </a>
+        <a
+          href={'http://patientsafetytech.com/'}
+          target="_blank"
+          className="relative w-[16rem] h-32"
+          rel="noreferrer"
+        >
           <Image
             src={'/assets/sponsorLogos/PRHI.png'}
             alt="PHRI"
             layout="fill"
             objectFit="contain"
           />
-        </div>
-        <div className="relative w-[24rem] h-20">
+        </a>
+        <a
+          href={'https://www.corelogic.com/culture/'}
+          target="_blank"
+          className="relative w-[24rem] h-20"
+          rel="noreferrer"
+        >
           <Image
             src={'/assets/sponsorLogos/CoreLogic.png'}
             alt="CoreLogic"
             layout="fill"
             objectFit="contain"
           />
-        </div>
+        </a>
+      </section>
+      <div className="flex justify-center 2xl:mt-20 md:mt-10 2xl:mb-0 md:mb-5">
+        <h4 className="text-center gold-text-gradient font-bold 2xl:text-4xl md:text-3xl text-2xl my-4">
+          Special thanks to
+        </h4>
+      </div>
+      <section className="flex flex-wrap justify-center items-center sm:space-y-0 > * + * space-y-6 > * + * p-4 2xl:space-x-12 > * + * md:space-x-6 > * + * sm:space-x-3 > * + *">
         <a
           href={'https://www.standoutstickers.com'}
           target="_blank"
@@ -104,6 +134,19 @@ export default function HomeSponsors() {
           <Image
             src={'/assets/sponsorLogos/StandOutStickers.jpg'}
             alt="StandOutStickers"
+            layout="fill"
+            objectFit="contain"
+          />
+        </a>
+        <a
+          href={'https://modernmarket.com/'}
+          target="_blank"
+          className="relative w-72 h-32"
+          rel="noreferrer"
+        >
+          <Image
+            src={'/assets/sponsorLogos/MME.jpeg'}
+            alt="Modern Market Eatery"
             layout="fill"
             objectFit="contain"
           />
