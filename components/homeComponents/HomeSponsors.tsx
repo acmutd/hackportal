84cdd58aaab86ff7c10366e2bb9d564e45a3e087
Amrit Rathie <amrit.rathie@gmail.com>
2023-10-12 05:20:29 +0000

    revert(schedule): Actually revert home component changes

--- components/homeComponents/HomeSponsors.tsx
+++ components/homeComponents/HomeSponsors.tsx
@@ -15,19 +15,6 @@ export default function HomeSponsors() {
       {/* Sponsor Cards */}
       <section className="flex flex-wrap justify-center items-center space-y-6 > * + * p-4 2xl:space-x-12 > * + * md:space-x-6 > * + * sm:space-x-3 > * + *">
         <a
-          href={'https://www.goldmansachs.com/careers/our-firm/engineering/index.html'}
-          target="_blank"
-          className="relative w-40 h-40 md:mt-6  md:mx-0 mx-8"
-          rel="noreferrer"
-        >
-          <Image
-            src={'/assets/sponsorLogos/GoldmanSachs.png'}
-            alt="GoldmanSachs"
-            layout="fill"
-            objectFit="contain"
-          />
-        </a>
-        <a
           href={'https://careers.toyota.com/us/en'}
           target="_blank"
           className="relative w-52 h-32"
@@ -41,7 +28,7 @@ export default function HomeSponsors() {
           />
         </a>
         <a
-          href={'https://leap.fidelitycareers.com'}
+          href={'Leap.FidelityCareers.com'}
           target="_blank"
           className="relative w-[22rem] h-20 "
           rel="noreferrer"
@@ -92,52 +79,22 @@ export default function HomeSponsors() {
             objectFit="contain"
           />
         </a>
-        <a
-          href={'https://geico.wd1.myworkdayjobs.com/External'}
-          target="_blank"
-          className="relative w-80 h-28"
-          rel="noreferrer"
-        >
-          <Image
-            src={'/assets/sponsorLogos/Geico.png'}
-            alt="Geico"
-            layout="fill"
-            objectFit="contain"
-          />
-        </a>
-        <a
-          href={'http://patientsafetytech.com/'}
-          target="_blank"
-          className="relative w-[16rem] h-32"
-          rel="noreferrer"
-        >
+        <div className="relative w-[16rem] h-32">
           <Image
             src={'/assets/sponsorLogos/PRHI.png'}
             alt="PHRI"
             layout="fill"
             objectFit="contain"
           />
-        </a>
-        <a
-          href={'https://www.corelogic.com/culture/'}
-          target="_blank"
-          className="relative w-[24rem] h-20"
-          rel="noreferrer"
-        >
+        </div>
+        <div className="relative w-[24rem] h-20">
           <Image
             src={'/assets/sponsorLogos/CoreLogic.png'}
             alt="CoreLogic"
             layout="fill"
             objectFit="contain"
           />
-        </a>
-      </section>
-      <div className="flex justify-center 2xl:mt-20 md:mt-10 2xl:mb-0 md:mb-5">
-        <h4 className="text-center gold-text-gradient font-bold 2xl:text-4xl md:text-3xl text-2xl my-4">
-          Special thanks to
-        </h4>
-      </div>
-      <section className="flex flex-wrap justify-center items-center sm:space-y-0 > * + * space-y-6 > * + * p-4 2xl:space-x-12 > * + * md:space-x-6 > * + * sm:space-x-3 > * + *">
+        </div>
         <a
           href={'https://www.standoutstickers.com'}
           target="_blank"
@@ -147,19 +104,6 @@ export default function HomeSponsors() {
           <Image
             src={'/assets/sponsorLogos/StandOutStickers.jpg'}
             alt="StandOutStickers"
-            layout="fill"
-            objectFit="contain"
-          />
-        </a>
-        <a
-          href={'https://modernmarket.com/'}
-          target="_blank"
-          className="relative w-72 h-32"
-          rel="noreferrer"
-        >
-          <Image
-            src={'/assets/sponsorLogos/MME.jpeg'}
-            alt="Modern Market Eatery"
             layout="fill"
             objectFit="contain"
           />
