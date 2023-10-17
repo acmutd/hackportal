ced01ded549578737e81180c21711430283fab91
Nicolas A. Burnett <nicolasandrewburnett@gmail.com>
2023-10-17 11:13:27 -0500

    fix: all done :D

--- pages/admin/scan/index.tsx
+++ pages/admin/scan/index.tsx
@@ -132,6 +132,7 @@ export default function Admin() {
           },
         },
         {
+          oldScanName: currentScan.name,
           scanData: updatedScanData,
         },
       );
--- pages/api/scan.tsx
+++ pages/api/scan.tsx
@@ -100,10 +100,10 @@ async function handleScan(req: NextApiRequest, res: NextApiResponse) {
       });
     }
 
-    if (scans.includes(bodyData.scan)) return res.status(201).json({ code: 'duplicate' });
-    const points = (snapshot.data().points ?? 0) + bodyData.netPoints
+    if (scans.includes(bodyData.scan) && !bodyData.isReclaimable) return res.status(201).json({ code: 'duplicate' });
+    const points = (snapshot.data().user.points ?? 0) + bodyData.netPoints
     if (!bodyData.isReclaimable) scans.push(bodyData.scan);
-    else if (points < 0) return res.status(418).json({ code: 'insufficient-points', message: `User does not have sufficient points: ${snapshot.data().points ?? 0}` })
+    else if (points < 0) return res.status(418).json({ code: 'insufficient-points', message: `User does not have sufficient points: ${snapshot.data().user.points ?? 0}` })
     await db.collection(REGISTRATION_COLLECTION).doc(bodyData.id).update({ scans, user: {
       ...snapshot.data().user,
       points
