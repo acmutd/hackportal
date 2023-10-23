21235b147236fbf810935181ff8eeec395bc7e4a
nam-t24 <namtruong831@gmail.com>
2023-10-23 15:52:55 -0500

    feat: Remove edit application from profile

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -111,15 +111,6 @@ export default function ProfilePage({ applicationDecisions }) {
               </p>
             </div>
           </div>
-          <button
-            type="button"
-            className="py-1 px-7 rounded-lg font-medium hover:bg-[#FFFCF9] bg-primaryDark text-secondary hover:text-primaryDark border-[1px] border-transparent hover:border-primaryDark transition duration-300 ease-in-out"
-            onClick={() => {
-              router.push('/register');
-            }}
-          >
-            Edit Application
-          </button>
         </div>
         {/* Info */}
         <div className="">
