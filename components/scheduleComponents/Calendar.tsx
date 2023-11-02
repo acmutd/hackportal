547fd642a20d75eda1a50e99f288b87cf376e7dd
Amrit Rathie <tirma.eithar@gmail.com>
2023-11-02 14:24:29 -0500

    fix(schedule): Make calendar grid rows longer

--- components/scheduleComponents/Calendar.tsx
+++ components/scheduleComponents/Calendar.tsx
@@ -294,7 +294,7 @@ function CalendarGrid({
         className="w-full overflow-x-auto overflow-y-hidden grid gap-0 auto-cols-[minmax(220px,1fr)] grid-cols-[max-content] font-secondary"
         style={
           {
-            gridTemplateRows: `repeat(${minutesInDay - startMin + increment}, 2px)`,
+            gridTemplateRows: `repeat(${minutesInDay - startMin + increment}, 2.45px)`,
           } as CSSProperties
         }
       >
